55bd54c0bda03cf592d7d2d1dd897cdc164b254b
VivekGarg2004 <66713544+VivekGarg2004@users.noreply.github.com>
2024-01-14 09:53:08 -0800

    Update Python_Script_API_Data_Collection.py

--- data/data_collection/python_data_collection/Python_Script_API_Data_Collection.py
+++ data/data_collection/python_data_collection/Python_Script_API_Data_Collection.py
@@ -1,12 +1,18 @@
 import Prizepicks_API_Helper
 import pandas as pd
-from datetime import date
+from datetime import datetime, timedelta
+import pytz
 import os
 
 api_url = "https://partner-api.prizepicks.com/projections?league_id=7&per_page=250&single_stat=true"
 today_data = Prizepicks_API_Helper.data_collection(api_url)
 
-date = date.today()
+desired_timezone = 'US/Pacific'
+tz = pytz.timezone(desired_timezone)
+current_date = datetime.now(tz)
+added_date = current_date + timedelta(hours=2)
+date = added_date.date()
+
 prop_list = []
 player_info_dict = {}
 
@@ -14,14 +20,15 @@ for player_entry in today_data['included']:
     player_id = player_entry['id']
 
     if player_entry['type'] == 'new_player':
-        player_name = player_entry['attributes']['name']
-        player_position = player_entry['attributes']['position']
-        team_name = player_entry['attributes']['team']
-        player_info_dict[player_id] = {
-            'name': player_name,
-            'position': player_position,
-            'team': team_name
-        }
+        if '+' not in player_entry['attributes']['name']:
+            player_name = player_entry['attributes']['name']
+            player_position = player_entry['attributes']['position']
+            team_name = player_entry['attributes']['team']
+            player_info_dict[player_id] = {
+                'name': player_name,
+                'position': player_position,
+                'team': team_name
+            }
 
 for data_entry in today_data['data']:
     player_id = data_entry['relationships']['new_player']['data']['id']
@@ -66,22 +73,22 @@ new_df = pd.DataFrame(prop_list)
 new_df['Player_ID'] = new_df['Player_ID'].astype(int)
 new_df['Line_Score'] = new_df['Line_Score'].astype(float)
 new_df.sort_values(['Start_Time', 'Game_ID', 'Name'], ascending=(True, True, True), inplace=True)
-print(new_df)
-
+new_df.head()
 
 # Joining the new data with the old data
 csv_name = f"{date}_NBA_Odds"
-folder_path = "/Users/vivekgarg/PycharmProjects/pythonProject2"
+folder_path = "/csv"
 file_name = f"{csv_name}.csv"
 file_path = os.path.join(folder_path, file_name)
 if os.path.exists(file_path):
     existing_df = pd.read_csv(file_path)
-    merged_df = existing_df.merge(new_df, on=['Player_ID', 'Name', 'Stat_Type', 'Position', 'Opponent', 'Team',
-                                              'Game_ID', 'Start_Time'], suffixes=('_existing', '_new'), how='outer')
+    merged_df = pd.merge(existing_df,new_df, on=['Player_ID', 'Name', 'Stat_Type', 'Position', 'Opponent', 'Team',
+                                                 'Game_ID', 'Start_Time'], suffixes=('_existing', '_new'), how='outer')
     merged_df['Line_Score_existing'] = merged_df['Line_Score_new'].combine_first(merged_df['Line_Score_existing'])
     merged_df = merged_df.drop(['Line_Score_new'], axis=1)
-    merged_df.sort_values(['Start_Time', 'Game_ID', 'Name'], ascending=(True, True, True), inplace=True)
-    print(merged_df)
-    merged_df.to_csv(file_name, index=False)
+    merged_df.sort_values(['Start_Time', 'Game_ID', 'Name','Stat_Type'], ascending=(True, True, True, True),
+                          inplace=True)
+    merged_df.head()
+    merged_df.to_csv(file_path, index=False)
 else:
-    new_df.to_csv(file_name, index=False)
+    new_df.to_csv(file_path, index=False)
